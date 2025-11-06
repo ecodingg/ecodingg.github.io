701f8fea8077769a4812b4620b5fe26c1ea5b6b2
eden <ecoding@pm.me>
2025-11-06 14:36:08 -0800

    Started reorganizing website

---
+++ personal_website/lib/homePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/cupertino.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  
+}
--- personal_website/lib/main.dart
+++ personal_website/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:personal_website/homePage.dart';
+import 'package:personal_website/projects.dart';
+import 'package:personal_website/shows.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PersonalWebsite());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PersonalWebsite extends StatelessWidget {
+  const PersonalWebsite({super.key});
 
   // This widget is the root of your application.
   @override
@@ -16,52 +21,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
       ),
       home: const MyHomePage(title: 'Welcome to my Web Page!'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('TabBar Sample'),
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.cloud_outlined)),
-              Tab(icon: Icon(Icons.beach_access_sharp)),
-              Tab(icon: Icon(Icons.brightness_5_sharp)),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            Center(child: Text("It's cloudy here")),
-            Center(child: Text("It's rainy here")),
-            Center(child: Text("It's sunny here")),
-          ],
-        ),
-      ),
     );
   }
 }
