31c58667de19b6b94cfb5ecd37bf3b254bf124f1
eden <ecoding@pm.me>
2025-11-06 14:47:37 -0800

    Moved homepage to main

--- personal_website/lib/main.dart
+++ personal_website/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:personal_website/homePage.dart';
 import 'package:personal_website/projects.dart';
 import 'package:personal_website/shows.dart';
 
@@ -21,6 +20,29 @@ class PersonalWebsite extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
       ),
       home: const MyHomePage(title: 'Welcome to my Web Page!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext build) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          shadowColor: const Color.fromARGB(255, 5, 253, 13),
+          title: const Text('Welcome to my website!'),
+        ),
+      ),
     );
   }
 }
